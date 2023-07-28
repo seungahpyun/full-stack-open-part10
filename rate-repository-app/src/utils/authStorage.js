837dc37068ea4574f6dca550dd03dff4bc563c23
seungahpyun <seungahpyun@icloud.com>
2023-07-28 11:18:04 +0200

    part10 10.14

---
+++ rate-repository-app/src/utils/authStorage.js
@@ -0,0 +1,25 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+class AuthStorage {
+  constructor(namespace = "auth") {
+    this.namespace = namespace;
+  }
+
+  async getAccessToken() {
+    const rawToken = await AsyncStorage.getItem(`${this.namespace}:token`);
+    return rawToken ? JSON.parse(rawToken) : "";
+  }
+
+  async setAccessToken(accessToken) {
+    const rawToken = JSON.stringify(accessToken);
+    await AsyncStorage.setItem(`${this.namespace}:token`, rawToken);
+
+    return accessToken;
+  }
+
+  async removeAccessToken() {
+    await AsyncStorage.removeItem(`${this.namespace}:token`);
+  }
+}
+
+export default AuthStorage;
