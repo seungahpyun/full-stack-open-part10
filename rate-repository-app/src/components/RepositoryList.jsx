e7c5b12a326f05040b9d1c34f5ae024f66e84267
seungahpyun <seungahpyun@icloud.com>
2023-07-31 14:51:58 +0200

    part10 10.23

--- rate-repository-app/src/components/RepositoryList.jsx
+++ rate-repository-app/src/components/RepositoryList.jsx
@@ -2,14 +2,22 @@ import { View, StyleSheet, FlatList } from 'react-native';
 import RepositoryItem from './RepositoryItem';
 import useRepositories from '../hooks/useRepositories';
 import { Link } from 'react-router-native';
+import React,{ useState } from 'react';
+import { Picker } from '@react-native-picker/picker';
 
 const styles = StyleSheet.create({
   separator: {
     height: 10,
   },
+  picker: {
+    backgroundColor: '#e0e0e0',
+    borderRadius: 5,
+    margin: 10,
+    padding: 10,
+  }
 });
 
-export const RepositoryListContainer = ({ repositories }) => {
+export const RepositoryListContainer = ({ repositories , order, setOrder }) => {
   const repositoryNodes = repositories
     ? repositories.edges.map((edge) => edge.node)
     : [];
@@ -24,16 +32,55 @@ export const RepositoryListContainer = ({ repositories }) => {
           </Link>
         )
         }
+        ListHeaderComponent={
+          <View>
+            <Picker
+              selectedValue={order}
+              onValueChange={(itemValue) => setOrder(itemValue)}
+              style={styles.picker}
+            >
+              <Picker.Item label="Latest repositories" value="latest" />
+              <Picker.Item label="Highest rated repositories" value="highest" />
+              <Picker.Item label="Lowest rated repositories" value="lowest" />
+            </Picker>
+          </View>
+        }
       />
     );
   };
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
-const RepositoryList = () => {
-  const { repositories } = useRepositories();
 
-  return <RepositoryListContainer repositories={repositories} />;
+const RepositoryList = () => {
+    const [ order, setOrder ] = useState('latest');
+
+    let orderBy;
+    let orderDirection;
+
+    switch(order){
+      case 'latest':
+
+        orderBy = 'CREATED_AT';
+        orderDirection = 'DESC';
+        break;
+
+      case 'highestRated':
+
+        orderBy = 'RATING_AVERAGE';
+        orderDirection = 'DESC';
+        break;
+
+      case 'lowestRated':
+
+        orderBy = 'RATING_AVERAGE';
+        orderDirection = 'ASC';
+        break;
+
+      }
+    const {repositories} = useRepositories(orderBy, orderDirection);
+
+  return <RepositoryListContainer repositories={repositories} order={order} setOrder={setOrder} />;
 };
 
 export default RepositoryList;
--- rate-repository-app/src/graphql/queries.js
+++ rate-repository-app/src/graphql/queries.js
@@ -2,8 +2,11 @@ import { gql } from "@apollo/client";
 import { REPO_DETAILS } from "./fragments";
 
 export const GET_REPOSITORIES = gql`
-  query {
-    repositories {
+  query Query(
+    $orderBy: AllRepositoriesOrderBy
+    $orderDirection: OrderDirection
+  ) {
+    repositories(orderBy: $orderBy, orderDirection: $orderDirection) {
       edges {
         node {
           ...RepoDetails
